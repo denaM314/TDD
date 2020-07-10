25ea82c972e03b4479a0dea749fc318421a8e59d
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-07-09 19:15:06 -0500

    initial commit

---
+++ TDDTestRun_Tests/Program_Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TDDTestRun_Tests
+{
+    public class Program_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
