e6dbed2e64287287c6fc2d54fb57c729f6956305
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-07-12 20:47:19 -0500

    Moved general tests to different class (refuction, negative denominator, 0 in denominator, etc)

---
+++ TDDTestRun_Tests/GeneralFractions_Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using TDDTestRun;
+
+namespace TDDTestRun_Tests
+{
+    class GeneralFractions_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ZeroDenominator()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Fraction(2, 0));
+        }
+
+        [Test]
+        public void FractionReducedOnCreation()
+        {
+            var result = new Fraction(2, 6);
+            Assert.IsTrue(result.Equals(new Fraction(1,3)));
+        }
+
+        [Test]
+        public void FractionReducedEqualToOneOnCreation()
+        {
+            var result = new Fraction(3, 3);
+            Assert.IsTrue(result.Equals(new Fraction(1)));
+        }
+
+        [Test]
+        public void NegativeDenominator_MadeIntoNegativeNumerator()
+        {
+            var result = new Fraction(2, -3);
+            Assert.IsTrue(result.Equals(new Fraction(-2, 3)));
+        }
+    }
+}
