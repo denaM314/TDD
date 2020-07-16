816e3d4f9cd43cbd555e14d78559d106398427a3
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-07-16 08:23:40 -0500

    Removed duplicated tests

--- TDDTestRun_Tests/GeneralFractions_Tests.cs
+++ TDDTestRun_Tests/GeneralFractions_Tests.cs
@@ -16,26 +16,5 @@ namespace TDDTestRun_Tests
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => new Fraction(2, 0));
         }
-
-        [Test]
-        public void FractionReducedOnCreation()
-        {
-            var result = new Fraction(2, 6);
-            Assert.AreEqual(new Fraction(1, 3), result);
-        }
-
-        [Test]
-        public void FractionReducedEqualToOneOnCreation()
-        {
-            var result = new Fraction(3, 3);
-            Assert.IsTrue(result.Equals(new Fraction(1)));
-        }
-
-        [Test]
-        public void NegativeDenominator_MadeIntoNegativeNumerator()
-        {
-            var result = new Fraction(2, -3);
-            Assert.IsTrue(result.Equals(new Fraction(-2, 3)));
-        }
     }
 }
