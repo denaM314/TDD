62f8f097248ef9691f352704427dc0de754bd268
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-07-15 20:22:08 -0500

    Added Equals Tests

--- TDDTestRun_Tests/FractionsEquals_Tests.cs
+++ TDDTestRun_Tests/FractionsEquals_Tests.cs
@@ -20,6 +20,18 @@ namespace TDDTestRun_Tests
         }
 
         [Test]
+        public void ReducedFraction()
+        {
+            Assert.AreEqual(new Fraction(12, 9), new Fraction(4, 3));
+        }
+
+        [Test]
+        public void ReducedToWholeNumber()
+        {
+            Assert.AreEqual(new Fraction(8, 4), new Fraction(2));
+        }
+
+        [Test]
         public void ReducedToOne()
         {
             Assert.AreEqual(new Fraction(4, 4), new Fraction(1));
@@ -29,6 +41,12 @@ namespace TDDTestRun_Tests
         public void ReducedToZero()
         {
             Assert.AreEqual(new Fraction(0, 5), new Fraction(0));
+        }
+
+        [Test]
+        public void DoubleNegative()
+        {
+            Assert.AreEqual(new Fraction(-3, -4), new Fraction(3, 4));
         }
     }
 }
