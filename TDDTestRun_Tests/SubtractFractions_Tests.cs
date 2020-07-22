a289455a14da7785e04bfaeb81ed7ae581d0f416
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-07-21 21:34:58 -0500

    Positive minus zero works

--- TDDTestRun_Tests/SubtractFractions_Tests.cs
+++ TDDTestRun_Tests/SubtractFractions_Tests.cs
@@ -11,10 +11,17 @@ namespace TDDTestRun_Tests
     class SubtractFractions_Tests
     {
         [Test]
-        public void ZeroFractions()
+        public void ZeroIntegers()
         {
             var result = new Fraction(0).Minus(new Fraction(0));
             Assert.AreEqual(new Fraction(0), result);
+        }
+
+        [Test]
+        public void Positive_minus_zero()
+        {
+            var result = new Fraction(2).Minus(new Fraction(0));
+            Assert.AreEqual(new Fraction(2), result);
         }
     }
 }
